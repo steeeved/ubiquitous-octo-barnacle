8d4f18824ca91ddcab2665647930ce59519a51df
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-20 02:40:53 +0300

    Clean code baby!

---
+++ src/Elements/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import Styles from './Navbar.module.scss';
+
+const Navbar = () => {
+  return (
+    <nav className={Styles}>
+      <h1>getCurrentWeather</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
