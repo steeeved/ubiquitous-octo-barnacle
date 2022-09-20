cb7a5d18f56e289c0108b680cc7508b21761fed1
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-20 12:40:18 +0300

    Firebase Failed

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,28 @@
 import video from './rain.mp4';
 import Styles from './Home.module.scss';
 
-export const Home = () => {
+import ErrorIcon from '@mui/icons-material/Error';
+import CloseIcon from '@mui/icons-material/Close';
+
+import { useState, useRef } from 'react';
+import classNames from 'classnames';
+
+// create an interface for the isError prop and setIsError function
+interface IHomeProps {
+  isError: boolean;
+  setIsError: (isError: boolean) => void;
+}
+
+
+export const Home = (props: IHomeProps) => {
+  const { isError, setIsError } = props;
+
   console.log('Home page rendered');
+
+  const errorClasses = classNames({
+    [Styles.error]: isError === true,
+    [Styles.noError]: isError === false
+  })
 
   const greeting = () => {
     const date = new Date();
@@ -28,13 +48,29 @@ export const Home = () => {
   const year = date.getFullYear();
 
   return (
-    <div className={Styles.main}>
+    <div className={Styles.mainG}>
       <div className={Styles.greeting}>
         <h1>{greeting()}</h1>
         <p>
           {day}, {dayOfMonth} {month} {year}
         </p>
       </div>
+
+      <div className={errorClasses}>
+        <div className={Styles.close} onClick={() => {setIsError(false)}}>
+          <CloseIcon />
+        </div>
+        <div className={Styles.info}>
+          <p>
+            Oops! Something went wrong.
+            <span>
+              To use this app, please enable location services in your browser
+              and refresh the page.
+            </span>
+          </p>
+        </div>
+      </div>
+
       <div className={Styles.video}>
         <video src={video} autoPlay loop muted />
       </div>
