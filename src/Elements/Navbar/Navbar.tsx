c93ac8d0170a6cf4b79da096ff235b2c1e9ea69a
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-20 13:15:08 +0300

    Yarn Baby!

--- src/Elements/Navbar/Navbar.tsx
+++ src/Elements/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
 import Styles from './Navbar.module.scss';
 
-const Navbar = () => {
+//set up interface for isError prop
+interface INavbarProps {
+  isError: boolean;
+}
+
+const Navbar = (props: INavbarProps) => {
+  const { isError } = props;
+  const barMSG = isError ? 'Error' : 'CurrentWeather';
   return (
     <nav className={Styles}>
-      <h1>getCurrentWeather</h1>
+      <h1>{barMSG}</h1>
     </nav>
   );
 };
