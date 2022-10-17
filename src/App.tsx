2e79c095af4128e13c43713906d4dcfe433aa135
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-18 00:10:58 +0300

    wrestle

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { Navbar } from './Elements';
 import { Home, About, Weather } from './Pages';
 import { ILongLat } from './Elements/Types';
+import { Iso } from '@material-ui/icons';
 
 function App() {
   const [isError, setIsError] = useState<boolean>(false);
@@ -20,32 +21,42 @@ function App() {
       latitude: position.coords.latitude
     });
     navigate('/weather');
+    setIsError(false);
   };
   const errorCallback = (error: any) => {
-    setIsError(true);
     navigate('/');
     console.log(error);
+    setIsError(true);
   };
+
+  // useEffect(() => {
+  //   if (
+  //     window.confirm('Please enable location services to use this app.') === false
+  //   ) {
+  //     setIsError(false);
+  //   }
+  // }, []);
+
+  console.log(isError);
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    if (
-      window.confirm('Please enable location services to use this app.') == true
-    ) {
-      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    }
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   }, []);
 
   return (
     <div className='App'>
-      <Navbar isError={ isError} />
+      <Navbar isError={isError} />
       <Routes>
         <Route
           path='/'
           element={<Home isError={isError} setIsError={setIsError} />}
         />
-        {isError === false && <Route path='/weather' element={<Weather longLat={longLat} />} />}
+        {isError === false && (
+          <Route path='/weather' element={<Weather longLat={longLat} />} />
+        )}
         <Route path='/about' element={<About />} />
+        <Route path='*' element={<h1 style={{ color: 'red' }}>Check where you are going mate</h1>} />
       </Routes>
     </div>
   );
